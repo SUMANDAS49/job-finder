ea27364ee48ba9cebcef64a32216d42820b91f06
Subhajeet Dey <subhajeetdey07@gmail.com>
2022-07-21 17:29:08 +0600

    update, delete post and filter

--- backend/controllers/Post.js
+++ backend/controllers/Post.js
@@ -1,5 +1,6 @@
 import Post from "../models/Post.js";
 
+// Post a new opening
 export const postAJob = async (req, res) => {
   const { company, jobTitle, jobDescription, skills, salary, jobType } =
     req.body;
@@ -17,7 +18,37 @@ export const postAJob = async (req, res) => {
   res.status(200).json(createdPost);
 };
 
+// fetching all the posts from db
 export const getAllJobs = async (req, res) => {
   const data = await Post.find({}).populate("userId");
   res.json(data);
+};
+
+// search jobs by skill
+export const searchJobsBySkill = async (req, res) => {
+  const relevantJobs = await Post.find({
+    skills : {"$in" : [req.body.skill]}
+  })
+  res.status(200).json(relevantJobs);
+};
+
+// search jobs by company
+export const searchJobsByCompany = async (req, res) => {
+  const relevantJobs = await Post.find({
+    company : req.params.company
+  })
+  res.status(200).json(relevantJobs);
+};
+
+// delete job
+export const deleteAjob = async (req, res) => {
+  const job = await Post.findById(req.params.id)
+  // console.log(job.userId)
+  // console.log(req.user._id)
+  if(job && job.userId.equals(req.user._id)) {
+    const deletedJob = await job.remove();
+    res.status(200).json(deletedJob);
+  } else {
+    res.status(501).json({msg : 'Not authorized'});
+  }
 };
--- backend/routes/Post.js
+++ backend/routes/Post.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { getAllJobs, postAJob } from "../controllers/Post.js";
+import { deleteAjob, getAllJobs, postAJob, searchJobsByCompany, searchJobsBySkill } from "../controllers/Post.js";
 import { protect } from "../middleWare/authMiddlewre.js";
 
 const router = express.Router();
 
 router.post("/create", protect, postAJob);
 router.get("/allPosts", getAllJobs);
+router.post("/searchSkill", searchJobsBySkill);
+router.get("/:company", searchJobsByCompany);
+router.delete("/delete/:id", protect ,deleteAjob);
 // router.post("/update");
 // router.get("/delete");
 
